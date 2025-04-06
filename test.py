96cde0d39b1fb3fb3de944d78d7203251ce38a6b
Anshika-jain01 <anshikajain0106@gmail.com>
2025-04-06 23:40:56 +0530

    add chages

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ team = Team(
         "openai", 
         "ollama",  
         "gemini-1.5-flash-8b", 
-        "llama3", 
+        "tinyllama", 
         system_message_1="You are a helpful assistant that can review travel plans, providing feedback on important/critical tips about how best to address language or communication challenges for the given destination. If the plan already includes language tips, you can mention that the plan is satisfactory, with rationale.",
         system_message_2="You are a helpful assistant that can take in all of the suggestions and advice from the other agents and provide a detailed final travel plan. You must ensure that the final plan is integrated and complete. YOUR FINAL RESPONSE MUST BE THE COMPLETE PLAN. When the plan is complete and all perspectives are integrated, you can respond with TERMINATE.",
         description_1="A helpful assistant that can plan trips.",
